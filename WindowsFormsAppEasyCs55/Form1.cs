c9841d0775ccaf4109e190098b50487f137f85d9
hideru <jota.16.stigma.world@gmail.com>
2021-11-04 04:58:38 +0900

    display succeeded

---
+++ WindowsFormsAppEasyCs55/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEasyCs55
+{
+    public partial class Form1 : Form
+    {
+        public void Display()
+        {
+            Application.Run(new Form1());
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "サンプル";
+            this.Width = 400;
+            this.Height = 200;
+            this.BackgroundImage = Image.FromFile("C:\\Users\\Enin\\RiderProjects\\WindowsFormsAppEasyCs55\\WindowsFormsAppEasyCs55\\img\\sunSymbol3.png");
+        }
+    }
+}
